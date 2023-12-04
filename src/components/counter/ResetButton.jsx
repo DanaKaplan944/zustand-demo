7ee2f8703e9e92f5a66176c1d24942f5427c619c
Dana Kaplan <danakaplan96@gmail.com>
2023-12-03 20:56:54 -0500

    with delays

--- src/components/counter/ResetButton.jsx
+++ src/components/counter/ResetButton.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
 import useStore from '../../store';
 
 const ResetButton = () => {
-  const { resetCount } = useStore();
+  const { doDelay, resetCount } = useStore();
   return (
     <div className='card'>
       <button className='reset-button' onClick={resetCount}>
         reset
+      </button>
+      <button className='reset-button' onClick={() => doDelay(10000)}>
+        medium delay
+      </button>
+      <button className='reset-button' onClick={() => doDelay(30000)}>
+        long delay
       </button>
     </div>
   );
